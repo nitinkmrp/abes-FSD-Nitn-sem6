9c9840a397821d3c56c39e23372fda65ddb7bc7e
nitinkmrp <nitinkmrp@gmail.com>
2026-01-28 14:57:48 +0530

    first commit

---
+++ DAY1/PROMISES/index.js
@@ -0,0 +1,11 @@
+const myPromise = new Promise((resolve, reject) => {
+    let marks = 75;
+    if (marks >= 60) {
+        resolve("You have passed the exam!");
+    } else {
+        reject("You have failed the exam.");
+    }   
+});
+myPromise.then((message) => 
+        console.log("Success: " + message)).catch((message) => 
+        console.log("Error: " + message))  ;
