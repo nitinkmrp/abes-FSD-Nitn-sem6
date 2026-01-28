74e2b50ae7081c6272ea4785985849f77460255e
nitinkmrp <nitin.23b0131204@abes.ac.in>
2026-01-28 15:23:16 +0530

    promises

--- DAY1/PROMISES/index.js
+++ DAY1/PROMISES/index.js
@@ -1,11 +1,28 @@
-const myPromise = new Promise((resolve, reject) => {
-    let marks = 75;
-    if (marks >= 60) {
-        resolve("You have passed the exam!");
-    } else {
-        reject("You have failed the exam.");
-    }   
-})
-myPromise.then((message) => 
-        console.log("Success: " + message)).catch((message) => 
-        console.log("Error: " + message))  
+// const myPromise = new Promise((resolve, reject) => {
+//     let marks = 75;
+//     if (marks >= 60) {
+//         resolve("You have passed the exam!");
+//     } else {
+//         reject("You have failed the exam.");
+//     }   
+// })
+// myPromise.then((message) => 
+//         console.log("Success: " + message)).catch((message) => 
+//         console.log("Error: " + message))  
+
+// const f1 = async () => {
+//     try {
+//         const message = await myPromise;   
+//         console.log(message);  
+//     } catch (message) {
+//         console.log(message);
+//     }   }   
+// f1()
+
+const getData = async () => {   
+    const response = await fetch('https://fakestoreapi.com/products');
+            
+    const res = await response.json();
+    console.log(res);
+}
+getData();
