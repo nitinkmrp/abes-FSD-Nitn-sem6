e87da61f50a4a51c94c2baa82695825d124bc966
nitinkmrp <nitin.23b0131204@abes.ac.in>
2026-02-11 15:16:37 +0530

    status

--- DAY3/dirsnc.js
+++ DAY3/dirsnc.js
@@ -1,11 +1,35 @@
 import fs from 'fs';
 
-const makeDirSync=(path)=>{
+// const makeDirSync=(path)=>{
+//     try {
+//         fs.mkdirSync(path);
+//         console.log("Directory created successfully");
+//     } catch (error) {
+//         console.log("Error creating directory:", error);
+//     }
+// }   
+// makeDirSync("../DAY5");
+
+
+// const readDirSync=(path)=>{
+//     try {
+//         fs.readdirSync(path);
+//         console.log("Directory read successfully");
+//         console.log("Contents of directory:", fs.readdirSync(path));    
+//     } catch (error) {
+//         console.log("Error reading directory:", error);
+//     }
+// }   
+// readDirSync("../DAY5");
+
+const removeDirSync=(path)=>{
     try {
-        fs.mkdirSync(path);
-        console.log("Directory created successfully");
+        fs.rmdirSync(path);
+        console.log("Directory removed successfully");
     } catch (error) {
-        console.log("Error creating directory:", error);
+        console.log("Error removing directory:", error);
     }
 }   
-makeDirSync("../DAY5");
+removeDirSync("../DAY5");
+
+
---
+++ DAY3/stat.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+const filestat=async(path)=>{
+    try {
+        const stats=await fs.promises.stat(path);
+        console.log(" stats.size:", stats.size);
+        console.log(" stats.isFile:", stats.isFile());
+        console.log(" stats.isDirectory:", stats.isDirectory());
+    } catch (error) {
+        console.log("Error getting file stats:", error);
+    }   
+}
+filestat("../DAY3/dir.js");
