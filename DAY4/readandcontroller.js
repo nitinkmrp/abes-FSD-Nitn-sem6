cd39304a505074b6b6989c5325ed7ad50e5fff46
nitinkmrp <nitin.23b0131204@abes.ac.in>
2026-02-16 13:09:11 +0530

    read write file using import and export function

--- DAY4/readandcontroller.js
+++ DAY4/readandcontroller.js
@@ -1,13 +1,22 @@
 import fs from "fs/promises";
 import { readFile,writeFile } from "./readandwritefile.js";
 
-
+const data = {
+  "id": 13,
+  "first_name": "nitin kumar",
+  "last_name": "Edards",
+  "email": "bedards0@amafihjhfs.com",
+  "gender": "Male"
+}
 
 const writeFileData = async (path, data) => {
     try {
         const fileData = await readFile(path, data);
-        const updatedData = [...fileData, data];
+        const updatedData = [];
+        if (!fileData) updatedData=[{id:1,...data   }];
+        else updatedData = [...fileData, {id:fileData.length+1,...data}];
         await writeFile(path, JSON.stringify(updatedData, null, 2));
+        console.log("File written successfully");
     } catch (error) {
         console.log("Error writing file:", error);
 
